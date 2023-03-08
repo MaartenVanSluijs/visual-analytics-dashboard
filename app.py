a84a3c064d3e0cbe85a614fb9b3abcf7bb9ddb34
LukavdBoogaard <64198098+LukavdBoogaard@users.noreply.github.com>
2023-03-08 17:20:26 +0100

    Changed code structure and added files for MC1

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from App.main import app
+from src.main import app
 from dash import html, ctx
 from dash.dependencies import Input, Output
+from src.plots.MC1 import MC1
+
 
 if __name__ == '__main__':
+    mc1 = MC1("mc1")
+
     # Create the app
     app.layout = html.Div(
         id="app-container",
         children=[
+            html.Div(
+                id="MC1",
+                className="nine columns",
+                children=[
+                    mc1
+
+                ]
+            )
             
         ],
     )
---
+++ src/plots/MC1.py
@@ -0,0 +1,24 @@
+from dash import dcc, html
+import plotly.express as px
+import json
+import numpy as np
+
+class MC1(html.Div):
+    def __init__(self, name):
+        self.html_id = name.lower().replace(" ", "-")
+    
+        # Equivalent to `html.Div([...])`
+        super().__init__(
+            className="graph_card",
+            children=[
+                dcc.Graph(id=self.html_id)
+            ],
+        )
+        # Load in the raw image data
+        self.data = self.get_data()
+
+    def get_data(self):
+        print("Hello World!")
+    
+    def update(self):
+        print("Hello World!")
