f04fa85622c177e693df5e3daa8089f3d60c1e87
Maarten van Sluijs <72663441+MaartenVanSluijs@users.noreply.github.com>
2023-03-17 19:22:52 +0100

    Added months column to data

--- data/MC1/data_cleanup.py
+++ data/MC1/data_cleanup.py
@@ -3,7 +3,7 @@ from dash import dcc, html
 import plotly.express as px
 
 def process_data():
-    df = pd.read_csv("data/SensorData.csv")
+    df = pd.read_csv("C:/Users/20182667/Documents/Studie/Vakken/Master/Visual Analytics/visual-analytics-dashboard/data/MC1/SensorData.csv")
     print(df.head())
 
     # set types
@@ -19,9 +19,14 @@ def process_data():
     print(df["gate-name"].value_counts())
     # There are no weird values in the columns
 
+    print(df["car-type"].nunique())
      
-    fig = px.line(df, x="Timestamp")
-    fig.show()
+    # fig = px.line(df, x="Timestamp")
+    # fig.show()
     # doesn't seem to be any gaps in the data 
+
+    df["month"] = df["Timestamp"].dt.month
+    df.to_csv("C:/Users/20182667/Documents/Studie/Vakken/Master/Visual Analytics/visual-analytics-dashboard/data/MC1/SensorData.csv",
+              index=False)
 
 process_data()
